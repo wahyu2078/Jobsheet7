1c887bf66826ef0d0ed8d2d80c416e61080b0104
wahyu2078 <wahyutptp20@gmail.com>
2023-10-12 10:02:07 +0700

    modif_DoWhileCuti29

--- DoWhileCuti29.java
+++ DoWhileCuti29.java
@@ -6,25 +6,23 @@ public class DoWhileCuti29 {
 
         int jatahCuti, jumlahHari;
         String konfirmasi;
-        
-        System.out.print("Jatah cuti: "); 
-        
+
+        System.out.print("Jatah cuti: ");
         jatahCuti = scan.nextInt();
-        
+
         do {
             System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
             konfirmasi = scan.next();
-        
+
             if (konfirmasi.equalsIgnoreCase("y")) {
                 System.out.print("Jumlah hari: ");
-                jumlahHari = scan.nextInt(); 
-                
+                jumlahHari = scan.nextInt();
+
                 if (jumlahHari <= jatahCuti) {
                     jatahCuti -= jumlahHari;
                     System.out.println("Sisa jatah cuti: " + jatahCuti);
                 } else {
                     System.out.println("Sisa jatah cuti Anda tidak mencukupi");
-                    break;
                 }
             }
         } while (jatahCuti > 0);
