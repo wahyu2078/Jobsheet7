b809f41e75d5230cdd4050240b1274e20ad03a32
wahyu2078 <wahyutptp20@gmail.com>
2023-10-12 08:36:24 +0700

    WhileGaji29

---
+++ WhileGaji29.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class WhileGaji29 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jumlahKaryawan = 0, jumlahJamLembur = 0;
+        double gajiLembur = 0.0, totalGajiLembur = 0.0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan : ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i + 1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+        }
+    }
+}
